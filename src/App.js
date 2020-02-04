c4e874900fcaa81b7f6a98313b613ced8aebb399
AhmedAGadir <ahmed.gadir@gmail.com>
2020-02-04 14:18:10 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { AgGridReact } from 'ag-grid-react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columnDefs: [
+        { headerName: "Make", field: "make" },
+        { headerName: "Model", field: "model" },
+        { headerName: "Price", field: "price" }
+
+      ],
+      rowData: [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxter", price: 72000 }
+      ],
+      gridLoaded: false
+    }
+  }
+
+  onGridReady = params => {
+    this.api = params.api;
+    this.columnApi = params.columnApi;
+
+    this.setState({ gridLoaded: true });
+  };
+
+  handleSelectAll = event => {
+    this.api.selectAll()
+  };
+
+  handleDeselectAll = event => {
+    this.api.deselectAll()
+  };
+
+  render() {
+    return (
+      <div>
+        <p data-testid="api">{this.state.gridLoaded ? 'the grid API has been loaded' : null}</p>
+        <button data-testid="selectAll" onClick={this.handleSelectAll}>Select All Rows</button>
+        <button data-testid="deSelectAll" onClick={this.handleDeselectAll}>Deselect All Rows</button>
+        <div
+          className="ag-theme-balham"
+          style={{
+            height: '500px',
+            width: '600px'
+          }}>
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            rowData={this.state.rowData}
+            onGridReady={this.onGridReady}>
+          </AgGridReact>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait } from '@testing-library/react';
+import { getByTestId, fireEvent } from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+// you probably want to have some sort of timeout here so this doesn't hang in the event of some
+// other grid related issue
+const ensureGridApiHasBeenSet = (component) => {
+  return new Promise(function (resolve, reject) {
+    (function waitForGridReady() {
+      if (getByTestId(component.container, 'api').innerHTML !== '') {
+        return resolve();
+      }
+      setTimeout(waitForGridReady, 100);
+    })();
+  });
+};
+
+
+describe('row selection', () => {
+
+  let appComponent = null;
+
+  beforeEach(async () => {
+    appComponent = render(<App />);
+    await wait(() => ensureGridApiHasBeenSet(appComponent));
+  });
+
+  afterEach(() => {
+    appComponent.unmount();
+  })
+
+  test('all rows selected', () => {
+    let selectAllButton = getByTestId(appComponent.container, 'selectAll');
+    fireEvent.click(selectAllButton);
+    let selectedNodesLength = appComponent.container.querySelectorAll('.ag-center-cols-container .ag-row-selected').length;
+    expect(selectedNodesLength).toBe(3);
+  });
+
+  test('all rows deselected', () => {
+    let deSelectAllButton = getByTestId(appComponent.container, 'deSelectAll');
+    fireEvent.click(deSelectAllButton);
+    let selectedNodesLength = appComponent.container.querySelectorAll('.ag-center-cols-container .ag-row-selected').length;
+    expect(selectedNodesLength).toBe(0);
+  });
+
 });
